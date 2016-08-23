d749ab346ccc2200dee3cbfce6392b7a4f556182
Joshua Carroll <joshcarroll21@gmail.com>
2016-08-23 13:54:02 -0500

    Turned on error reporting

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<!DOCTYPE html>
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?><!DOCTYPE html>
 <html>
 	<head>
 		<title>Garage Opener</title>
