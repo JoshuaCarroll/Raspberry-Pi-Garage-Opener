9d44eb5e9d25d992146e40ce64872125627c0300
Joshua Carroll <joshcarroll21@gmail.com>
2016-08-23 14:28:13 -0500

    Status exposed as JSON

--- status.php
+++ status.php
@@ -4,6 +4,6 @@ include "filename.php";
 $fileReader = fopen($filename, "r+") or die("Unable to open file for reading.");
 $status = trim(fread($fileReader,filesize($filename)));
 fclose($fileReader);
-echo $status;
+echo "{ 'status' : '" . $status . "' }";
 
 ?>
