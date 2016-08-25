804db1fcf82c132645cb82206f7ca8d3647bcdb8
Joshua Carroll <joshcarroll21@gmail.com>
2016-08-24 21:15:31 -0500

    Added error reporting

--- utilities.php
+++ utilities.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Database connection settings
 class DbSettings {
     public static $Address = '127.0.0.1';
